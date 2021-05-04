2730a653ccf9660c84c3f712b831da3752a0c851
aliya-w <80278648+aliya-w@users.noreply.github.com>
2021-05-04 04:24:40 -0600

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,36 @@
+print("Hello, World");
+
+if 17 > 10:
+    print("seventeen");
+
+
+#who wants to be a Millionare?
+
+
+
+
+
+""""wim
+
+jbkj
+vm
+nbvmn
+mvkbl
+sc"""
+
+
+x= "I AM A"
+y= "A coder"
+print(x+y)
+
+x=20.71
+y=3454
+print(x+y)
+
+
+x=" potato"
+print(" i am" + x)
+
+
+
+
